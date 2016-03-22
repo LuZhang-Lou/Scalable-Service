39288745c79aa1032c20de79fa47d47905158897
LuZhang-Lou <lzhang3@andrew.cmu.edu>
2016-03-21 22:48:21 -0400

    some improvement

--- sample/Server.java
+++ sample/Server.java
@@ -6,9 +6,7 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 public class Server extends UnicastRemoteObject
         implements ServerIntf  {
@@ -26,7 +24,11 @@ public class Server extends UnicastRemoteObject
 	private static String selfIP;
 	private static ServerLib SL;
 	private static int curRound = 0;
-	private static int numOfApps = 3;
+//	private static int numOfApps = 3; // 0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22  23
+	private static byte[] appInitConf = {2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 6, 6, 6, 6};
+	private static byte[] forInitConf = {1, 1, 1, 1, 1, 1, 2, 2, 4, 4, 4, 4, 2, 2, 2, 2, 4, 4, 4, 4, 6, 6, 6, 6};
+	private static LinkedList<Long> stats;
+	private static long lastScaleoutTime;
 	protected Server() throws RemoteException {
 	}
 
@@ -52,28 +54,26 @@ public class Server extends UnicastRemoteObject
 			selfRole = FORWARDER;
 			appServerList = new ArrayList<>();
 			forServerList = new ArrayList<>();
+			stats = new LinkedList<>();
 			// provision machines according to current time
 			// current master is not responsible for front work.
 
-//			float curTime = SL.getTime();
-//			int num = getVMNumber(curTime);
-//			for (int i = 0; i < num-1; ++i){
-//				SL.startVM();
-//			}
-
+			float curTime = SL.getTime();
+			byte[] nums = getVMNumber(curTime);
 
 			// launch 3 appServer
-			for (int i = 0; i < 3; ++i){
+			for (int i = 0; i < nums[1]; ++i){
 				System.out.println("launching apps..");
 				appServerList.add(SL.startVM() + basePort);
 			}
 
 //			Thread.sleep(4500);
 			// launch 1 Forward
-			for (int i = 0; i < 2; ++i){
+			for (int i = 0; i < nums[0]; ++i){
 				System.out.println("launching fors..");
 				forServerList.add(SL.startVM() + basePort);
 			}
+
 		} else { // non-masetr // ask for role.
 			try {
 				masterIntf = (ServerIntf) Naming.lookup(String.format("//%s:%d/server", selfIP, basePort +1));
@@ -86,13 +86,35 @@ public class Server extends UnicastRemoteObject
             }
         }
 
-
 		if (selfRole == FORWARDER){
 			SL.register_frontend();
+			while (SL.getStatusVM(2) == Cloud.CloudOps.VMStatus.Booting){
+				Cloud.FrontEndOps.Request r = SL.getNextRequest();
+				SL.drop(r);
+			}
 			while (true){
 				Cloud.FrontEndOps.Request r = SL.getNextRequest();
 				long timeConsumed = forwardReq(r);
 				System.out.println("timeConsumed:" + timeConsumed);
+
+
+                if (vmId == 1){ // scale
+					//scaleOut()
+					System.out.println("length:" + SL.getQueueLength());
+					stats.addLast(timeConsumed);
+					if (stats.size() > 5){
+						stats.removeFirst();
+						OptionalDouble avgTime = stats.stream().mapToLong(p -> p).average();
+
+						if ((System.currentTimeMillis() - lastScaleoutTime)/1000 > 60 &&
+								avgTime.getAsDouble() > 300){
+							System.out.println("Now Scale out.");
+							scaleOut(1);
+							lastScaleoutTime = System.currentTimeMillis();
+						}
+					}
+				}
+
 			}
 		} else { // processor
 			while (true){
@@ -101,24 +123,24 @@ public class Server extends UnicastRemoteObject
 		}
 	}
 
+	public static long scaleOut(int num){
+		return 0;
+	}
 
 	public static long forwardReq(Cloud.FrontEndOps.Request r) throws Exception{
-		int RPCPort = appServerList.get(curRound++);
-//		boolean retry = true;
+		int RPCPort = appServerList.get(curRound);
 		ServerIntf curAppIntf = null;
 		while (true){
             try {
                 curAppIntf = (ServerIntf) Naming.lookup(String.format("//%s:%d/server", selfIP, RPCPort));
                 break;
-//				retry = false;
 			}catch (Exception e){
 //				Thread.sleep(100);
-//				retry = true;
 				continue;
             }
 		}
 		long timeConsumed = curAppIntf.processReq(r);
-		curRound %= numOfApps;
+		curRound = (curRound + 1) % appServerList.size();
 		return timeConsumed;
 	}
 
@@ -147,14 +169,19 @@ public class Server extends UnicastRemoteObject
 	 * @param curTime current time
 	 * @return number of needed vms
      */
-	private static int getVMNumber(float curTime){
-		if (curTime <= 7){
-			return 2;
+	private static byte[] getVMNumber(float curTime){
+		byte[] ret = new byte[2];
+		if (curTime <= 6){
+			ret[0] = 2;
+			ret[1] = 3;
+		}else if (curTime <= 16){
+			ret[0] = 3;
+			ret[1] = 4;
+		} else {
+			ret[0] = 5;
+			ret[1] = 10;
 		}
-		if (curTime <= 19){
-			return 4;
-		}
-		return 6;
+		return ret;
 	}
 }
 
