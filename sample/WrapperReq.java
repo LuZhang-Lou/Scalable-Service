213eef26f8f34c1499a946ae9030fded1ab4f3ee
LuZhang-Lou <lzhang3@andrew.cmu.edu>
2016-04-01 20:01:03 -0400

    centrailize v0.3

---
+++ sample/WrapperReq.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+/**
+ * Created by Lu on 4/1/16.
+ */
+public class WrapperReq implements Serializable {
+        long getTime;
+        Cloud.FrontEndOps.Request request;
+        boolean isPurchase;
+        WrapperReq(Cloud.FrontEndOps.Request r,long time){
+            this.request = r;
+            this.getTime = time;
+            this.isPurchase = r.isPurchase;
+        }
+        boolean isTimeout(){
+            if (isPurchase){
+                long period = System.currentTimeMillis() - getTime;
+                System.out.println("Purchase:" + period);
+                return (period > 1300);
+            }
+            long period = System.currentTimeMillis() - getTime;
+            System.out.println("NotPurchase:" + period);
+            return (period) > 560;
+        }
+
+    }
+
